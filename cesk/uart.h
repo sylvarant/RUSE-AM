c469579fc349a7480353318680570fd8b8de4106
Adriaan Larmuseau <adriaan.larmuseau@gmail.com>
2013-07-12 17:43:21 +0200

    Addin FPGA support

---
+++ cesk/uart.h
@@ -0,0 +1,18 @@
+#ifndef MSP_SUPPORT_UART_H
+#define MSP_SUPPORT_UART_H
+
+// SPM Code by Job
+
+#ifdef FPGA
+#include <stddef.h>
+
+void uart_init(void);
+int uart_available(void);
+void uart_write_byte(unsigned char b);
+void uart2_write_byte(unsigned char b);
+void uart_write(const unsigned char* buf, size_t size);
+unsigned char uart_read_byte(void);
+void uart_read(unsigned char* buf, size_t size);
+#endif
+
+#endif
